c4266d543c92497ef44f35f288dcdcbcfd3e5a88
npm okl <npmokl@gmx.com>
2020-03-09 20:06:47 +0000

    feat: add equidistant grid creation and helper functions, d_rms function, centroid (center of gravity) test

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const nearPointRemoval = require("./lib/nearPointRemoval");
 const outlierPointRemoval = require("./lib/outlierPointRemoval");
+const { centroid } = require("./lib/centroid");
+const { distancesFromCenter, drms } = require("./lib/drms");
 const { HAVERSINE, VINCENTY } = require("./lib/constants");
+const {
+  createArray,
+  createBoundingBoxWithDistance,
+  createGrid,
+  getBoundingBox,
+  getExpandedCell,
+  getGridCellForCoordinates,
+  getGridCoordinatesFlat
+} = require("./lib/grid");
 module.exports = {
-  nearPointRemoval,
-  outlierPointRemoval,
   HAVERSINE,
-  VINCENTY
+  VINCENTY,
+  centroid,
+  createArray,
+  createGrid,
+  distancesFromCenter,
+  drms,
+  getBoundingBox,
+  getExpandedCell,
+  getGridCellForCoordinates,
+  getGridCoordinatesFlat,
+  nearPointRemoval,
+  outlierPointRemoval
 };
